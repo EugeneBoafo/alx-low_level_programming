2c67002389e4fb2078ab426bfd4f3561c9dea8a8
myusername <myemail@gmail.com>
2023-03-09 03:12:22 -0800

    instructions

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,16 +1,31 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - reverse
- * @s: pointer to string params
- *
+ * wildcmp - Compare strings
+ * @s1: pointer to string params
+ * @s2: pointer to string params
+ * Return: 0
  */
 
-void _print_rev_recursion(char *s)
+int wildcmp(char *s1, char *s2)
 {
-	if (*s)
+	if (*s1 == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		if (*s2 != '\0' && *s2 == '*')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
 	}
+
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+	}
+	else if (*s1 == *s2)
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
 }
+
