7ae64ef023b6516b8a05220b0e20f8d06fbec158
myusername <myemail@gmail.com>
2023-03-21 14:39:34 -0700

    2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - preints the name of the file
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
@@ -10,3 +10,5 @@ int main(void)
 	printf("%s\n", __FILE__);
 	return (0);
 }
+
+
